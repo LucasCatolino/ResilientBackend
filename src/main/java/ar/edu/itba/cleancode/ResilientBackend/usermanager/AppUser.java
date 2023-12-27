f5a25ea3ad06e0d2e98fffcd4f359ed9895c6d36
Lucas <lcatolino@fi.uba.ar>
2023-12-27 12:03:44 -0300

    Change: other resilient strategies added

--- src/main/java/ar/edu/itba/cleancode/ResilientBackend/usermanager/AppUser.java
+++ src/main/java/ar/edu/itba/cleancode/ResilientBackend/usermanager/AppUser.java
@@ -1,6 +1,9 @@
 package ar.edu.itba.cleancode.resilientbackend.usermanager;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,6 +11,8 @@ import jakarta.persistence.Id;
 
 @Entity
 public class AppUser {
+
+    private static final String BACKENDUSERS = "backendusers";
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,51 +23,82 @@ public class AppUser {
     private String birthday;
     private String password;
 
-    public String getPassword() {
-        return this.password;
-    }
-
-    @CircuitBreaker (name = "backendUsers")
+    @CircuitBreaker (name = BACKENDUSERS)
+    @TimeLimiter(name = BACKENDUSERS)
+    @Bulkhead(name = BACKENDUSERS)
+    @Retry(name = BACKENDUSERS)
     public Long getId() {
         return this.id;
     }
 
-    @CircuitBreaker (name = "backendUsers")
+    @CircuitBreaker (name = BACKENDUSERS)
+    @TimeLimiter(name = BACKENDUSERS)
+    @Bulkhead(name = BACKENDUSERS)
+    @Retry(name = BACKENDUSERS)
     public void setId(Long id) {
         this.id = id;
     }
 
-    @CircuitBreaker (name = "backendUsers")
+    @CircuitBreaker (name = BACKENDUSERS)
+    @TimeLimiter(name = BACKENDUSERS)
+    @Bulkhead(name = BACKENDUSERS)
+    @Retry(name = BACKENDUSERS)
     public String getName() {
         return this.name;
     }
 
-    @CircuitBreaker (name = "backendUsers")
+    @CircuitBreaker (name = BACKENDUSERS)
+    @TimeLimiter(name = BACKENDUSERS)
+    @Bulkhead(name = BACKENDUSERS)
+    @Retry(name = BACKENDUSERS)
     public void setName(String name) {
         this.name = name;
     }
 
-    @CircuitBreaker (name = "backendUsers")
+    @CircuitBreaker (name = BACKENDUSERS)
+    @TimeLimiter(name = BACKENDUSERS)
+    @Bulkhead(name = BACKENDUSERS)
+    @Retry(name = BACKENDUSERS)
     public String getMail() {
         return this.mail;
     }
 
-    @CircuitBreaker (name = "backendUsers")
+    @CircuitBreaker (name = BACKENDUSERS)
+    @TimeLimiter(name = BACKENDUSERS)
+    @Bulkhead(name = BACKENDUSERS)
+    @Retry(name = BACKENDUSERS)
     public void setMail(String mail) {
         this.mail = mail;
     }
 
-    @CircuitBreaker (name = "backendUsers")
+    @CircuitBreaker (name = BACKENDUSERS)
+    @TimeLimiter(name = BACKENDUSERS)
+    @Bulkhead(name = BACKENDUSERS)
+    @Retry(name = BACKENDUSERS)
     public String getBirthday() {
         return this.birthday;
     }
 
-    @CircuitBreaker (name = "backendUsers")
+    @CircuitBreaker (name = BACKENDUSERS)
+    @TimeLimiter(name = BACKENDUSERS)
+    @Bulkhead(name = BACKENDUSERS)
+    @Retry(name = BACKENDUSERS)
     public void setBirthday(String birthday) {
         this.birthday = birthday;
     }
+    
+    @CircuitBreaker (name = BACKENDUSERS)
+    @TimeLimiter(name = BACKENDUSERS)
+    @Bulkhead(name = BACKENDUSERS)
+    @Retry(name = BACKENDUSERS)
+    public String getPassword() {
+        return this.password;
+    }
 
-    @CircuitBreaker (name = "backendUsers")
+    @CircuitBreaker (name = BACKENDUSERS)
+    @TimeLimiter(name = BACKENDUSERS)
+    @Bulkhead(name = BACKENDUSERS)
+    @Retry(name = BACKENDUSERS)
     public void setPassword(String password) {
         this.password = password;
     }
