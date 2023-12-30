33742bb290001bb426d9d404677a165b4debddbd
Lucas <lcatolino@fi.uba.ar>
2023-12-30 15:17:55 -0300

    Add: reaction Co-authored-by: german-ariel-martinez <german-ariel-martinez@users.noreply.github.com>

---
+++ src/main/java/ar/edu/itba/cleancode/ResilientBackend/reactionmanager/Reaction.java
@@ -0,0 +1,108 @@
+package ar.edu.itba.cleancode.resilientbackend.reactionmanager;
+
+import ar.edu.itba.cleancode.resilientbackend.usermanager.AppUser;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import ar.edu.itba.cleancode.resilientbackend.tweetmanager.Tweet;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Reaction {
+
+    private static final String BACKEND_REACT = "backendusers";
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reactionId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private AppUser userId;
+
+    @ManyToOne
+    @JoinColumn(name = "tweetId", nullable = false)
+    private Tweet tweetId;
+
+    // True = Like & False = dislike
+    private Boolean is_like;
+
+    public Reaction(Boolean isLike, Long tweetId, Long userId) {
+        this.setLike(isLike);
+        this.setTweetId(tweetId);
+        this.setUserId(userId); // TODO: da error porque dice que AppUser.getId() da null por this.userId null
+                                /* ¿por qué con tweet funciona y con user no? */
+    }
+
+    @CircuitBreaker (name = BACKEND_REACT)
+    @TimeLimiter(name = BACKEND_REACT)
+    @Bulkhead(name = BACKEND_REACT)
+    @Retry(name = BACKEND_REACT)
+    public Long getId() {
+        return this.reactionId;
+    }
+
+    @CircuitBreaker (name = BACKEND_REACT)
+    @TimeLimiter(name = BACKEND_REACT)
+    @Bulkhead(name = BACKEND_REACT)
+    @Retry(name = BACKEND_REACT)
+    public AppUser getUserId() {
+        return this.userId;
+    }
+
+    @CircuitBreaker (name = BACKEND_REACT)
+    @TimeLimiter(name = BACKEND_REACT)
+    @Bulkhead(name = BACKEND_REACT)
+    @Retry(name = BACKEND_REACT)
+    public void setUserId(Long userId) {
+        AppUser user = new AppUser(userId);
+        /*user.setId(userId);*/
+        
+        this.userId = user;
+    }
+
+    @CircuitBreaker (name = BACKEND_REACT)
+    @TimeLimiter(name = BACKEND_REACT)
+    @Bulkhead(name = BACKEND_REACT)
+    @Retry(name = BACKEND_REACT)
+    public Tweet getTweetId() {
+        return this.tweetId;
+    }
+
+    @CircuitBreaker (name = BACKEND_REACT)
+    @TimeLimiter(name = BACKEND_REACT)
+    @Bulkhead(name = BACKEND_REACT)
+    @Retry(name = BACKEND_REACT)
+    public void setTweetId(Long tweetId) {
+        Tweet tweet = new Tweet();
+        tweet.setId(tweetId);
+        
+        this.tweetId = tweet;
+    }
+    
+    @CircuitBreaker (name = BACKEND_REACT)
+    @TimeLimiter(name = BACKEND_REACT)
+    @Bulkhead(name = BACKEND_REACT)
+    @Retry(name = BACKEND_REACT)
+    public Boolean getLike() {
+        return this.is_like;
+    }
+
+    @CircuitBreaker (name = BACKEND_REACT)
+    @TimeLimiter(name = BACKEND_REACT)
+    @Bulkhead(name = BACKEND_REACT)
+    @Retry(name = BACKEND_REACT)
+    public void setLike(Boolean newReaction) {
+        this.is_like = newReaction;
+    }
+
+}
