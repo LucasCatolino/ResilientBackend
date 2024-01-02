c46f18dccbb874fb1eea197186bc1dda0958d971
Lucas <lcatolino@fi.uba.ar>
2024-01-02 10:03:05 -0300

    Change: try catch when posting reaction

--- src/main/java/ar/edu/itba/cleancode/ResilientBackend/controllers/ReactionController.java
+++ src/main/java/ar/edu/itba/cleancode/ResilientBackend/controllers/ReactionController.java
@@ -3,11 +3,13 @@ package ar.edu.itba.cleancode.resilientbackend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.edu.itba.cleancode.resilientbackend.DatabaseConnector;
@@ -37,12 +39,18 @@ public class ReactionController {
     }
 
     @PostMapping("/reactions")
-    public Reaction addReaction(@RequestBody ReactionRequest request) {
+    @ResponseStatus(HttpStatus.OK)
+    public String addReaction(@RequestBody ReactionRequest request) {
         Reaction reaction = new Reaction();
         reaction.setLike(request.getLike());
         reaction.setTweetId(request.getTweetId());
         reaction.setUserId(request.getUserId());
-        return reactionRepository.save(reaction);
+        try {
+            reactionRepository.save(reaction);
+        } catch (Exception e) {
+            throw new Error("Error al guardar");
+        }
+        return "Reaction saved";
     }
 
 
